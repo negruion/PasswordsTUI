43c16fcc46e7e7ee6df89f4723f204a18e8b6e08
NegruIon <ionegru776@gmail.com>
2023-12-21 15:53:22 +0100

    add methods to show the commands

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,32 @@
 public class Main {
     public static void main(String[] args) {
 
+    Main main = new Main();
+    main.run();
+
     }
+
+    /** Runs the PasswordsTUI. */
+    public void run(){
+
+
+
+    }
+
+
+    public void showCommands(){
+        String commands =
+                """
+                generate            - generates a password
+                check <password>    - checks the given password
+                quit                - quits the PasswordsTUI
+                """;
+        System.out.println(commands);
+    }
+
+    public void showStartTUI(){
+        System.out.println("Welcome to PasswordsTUI!");
+        showCommands();
+    }
+
 }
