88d1d435e2a9d1780c8f27e8f0112d0850d42ea7
NegruIon <ionegru776@gmail.com>
2023-12-21 17:50:44 +0100

    add method comment and make code safer

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,53 @@
+import java.util.Scanner;
+import passwords.Checker;
+import passwords.Generator;
 
 public class Main {
+
+    boolean running = true;
+    Generator generator = new Generator();
+    Checker checker = new Checker();
+
     public static void main(String[] args) {
 
-    Main main = new Main();
-    main.run();
+        Main passwordsTUI = new Main();
+        passwordsTUI.run();
 
     }
 
     /** Runs the PasswordsTUI. */
     public void run(){
+        showStartTUI();
 
+        Scanner keyboard = new Scanner(System.in);
 
+        while(running){
+            System.out.print("Choice: ");
+            String command = keyboard.nextLine();
+
+            switch (command){
+                case "generate" -> {
+                    String password = generator.getPassword();
+                    System.out.println("Generated Password: " + password);
+                }
+                case "check" -> {
+
+                }
+                case "quit" -> {
+                    System.out.println("Quitting the TUI...");
+                    running = false;
+                }
+                default -> {
+                    System.out.println("You've entered an unknown command!");
+                    showCommands();
+                }
+
+            }
+        }
 
     }
 
-
+    /** Shows the available commands for the TUI. */
     public void showCommands(){
         String commands =
                 """
@@ -25,6 +58,7 @@ public class Main {
         System.out.println(commands);
     }
 
+    /** Shows the welcome message and the available commands for the TUI. */
     public void showStartTUI(){
         System.out.println("Welcome to PasswordsTUI!");
         showCommands();
