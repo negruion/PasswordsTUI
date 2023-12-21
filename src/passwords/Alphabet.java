550b6ad17b811ea9d4a6ff6acc3f00b61f93eb84
NegruIon <ionegru776@gmail.com>
2023-12-21 16:27:41 +0100

    add functionality to create a string with all allowed characters

--- src/passwords/Alphabet.java
+++ src/passwords/Alphabet.java
@@ -1,3 +1,30 @@
 package passwords;
 
-public class Alphabet { }
+public class Alphabet {
+
+    private static final String UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
+    private static final String SYMBOLS = "!@#$%^&*()-_=+\\/~?";
+
+    StringBuilder allowedChars;
+
+    public Alphabet(boolean upperLetters, boolean lowerLetters, boolean digits, boolean symbols){
+
+        allowedChars = new StringBuilder();
+
+        if(upperLetters) allowedChars.append(UPPERCASE_LETTERS);
+
+        if(lowerLetters) allowedChars.append(LOWERCASE_LETTERS);
+
+        if(digits) allowedChars.append(DIGITS);
+
+        if(symbols) allowedChars.append(SYMBOLS);
+
+    }
+
+    public String getAllowedChars(){
+        return this.allowedChars.toString();
+    }
+
+}
